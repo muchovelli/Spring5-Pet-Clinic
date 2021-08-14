ca7bbae3eb0fed743016729d28e0a3f5a36b0504
muchovelli <66911866+muchovelli@users.noreply.github.com>
2021-08-14 23:11:26 +0200

    Convert owners to jpa entities

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -3,8 +3,15 @@ package guru.springframework.sfgpetclinic.model;
 /**
  * Created by jt on 7/13/18.
  */
-public class PetType extends BaseEntity {
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="types")
+public class PetType extends BaseEntity {
+    @Column(name="name")
     private String name;
 
     public String getName() {
