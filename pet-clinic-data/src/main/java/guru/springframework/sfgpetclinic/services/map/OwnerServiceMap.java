f17ac00ddae23fe0a26dcd3e0ecb5177189254f2
muchovelli <66911866+muchovelli@users.noreply.github.com>
2021-08-15 14:20:01 +0200

    added spring data jpa service for owners

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -5,6 +5,7 @@ import guru.springframework.sfgpetclinic.model.Pet;
 import guru.springframework.sfgpetclinic.services.OwnerService;
 import guru.springframework.sfgpetclinic.services.PetService;
 import guru.springframework.sfgpetclinic.services.PetTypeService;
+import org.hibernate.mapping.Set;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
